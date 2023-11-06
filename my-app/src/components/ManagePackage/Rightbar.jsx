d96d6f805030e8442bb99fc1503fe720acc86ab8
giaducdang03 <giaducdang@gmail.com>
2023-11-07 01:02:01 +0700

    fix error

--- my-app/src/components/ManagePackage/Rightbar.jsx
+++ my-app/src/components/ManagePackage/Rightbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import config from "../../utils/cus-axios";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
