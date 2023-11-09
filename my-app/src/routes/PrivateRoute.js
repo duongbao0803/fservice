c5e9da2ef91307cd44b8e0d7a26a4c9c24e1c60d
duongbao0803 <tonbao0803@gmail.com>
2023-11-09 10:39:33 +0700

    update authorization role

--- my-app/src/routes/PrivateRoute.js
+++ my-app/src/routes/PrivateRoute.js
@@ -1,50 +1,56 @@
 import React, { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Session } from "../App";
 import NotFound from "../components/NotFound/NotFound";
+import HomePage from "../page/HomePage";
 
 const PrivateRoute = (props) => {
-  // const { user } = useContext(Session);
-  // console.log("check user", user);
+  //   if (
+  //     localStorage.getItem("role") === "ADMIN" &&
+  //     props.allowedRole.includes("ADMIN")
+  //   ) {
+  //     return (
+  //       <>
+  //         <div>{props.children}</div>;
+  //       </>
+  //     );
+  //   }
 
-  if (
-    localStorage.getItem("role") === "ADMIN" &&
-    props.allowedRole.includes("ADMIN")
-  ) {
-    return (
-      <>
-        <div>{props.children}</div>;
-      </>
-    );
+  //   if (
+  //     localStorage.getItem("role") === "USER" &&
+  //     props.allowedRole.includes("USER")
+  //   ) {
+  //     return (
+  //       <>
+  //         <div>{props.children}</div>;
+  //       </>
+  //     );
+  //   }
+
+  //   if (
+  //     localStorage.getItem("role") === "STAFF" &&
+  //     props.allowedRole.includes("STAFF")
+  //   ) {
+  //     return (
+  //       <>
+  //         <div>{props.children}</div>
+  //       </>
+  //     );
+  //   }
+
+  //   return (
+  //     <Routes>
+  //       <Route path="*" element={<HomePage />} />
+  //     </Routes>
+  //   );
+
+  const userRole = localStorage.getItem("role");
+
+  if (userRole && props.allowedRole.includes(userRole)) {
+    return <>{props.children}</>;
   }
 
-  if (
-    localStorage.getItem("role") === "USER" &&
-    props.allowedRole.includes("USER")
-  ) {
-    return (
-      <>
-        <div>{props.children}</div>;
-      </>
-    );
-  }
-
-  if (
-    localStorage.getItem("role") === "STAFF" &&
-    props.allowedRole.includes("STAFF")
-  ) {
-    return (
-      <>
-        <div>{props.children}</div>
-      </>
-    );
-  }
-
-  return (
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+  return <Navigate to="*" />;
 };
 
 export default PrivateRoute;
