b57665a6e23ef0e2e004b323977ef71aa1ab8014
giaducdang03 <giaducdang@gmail.com>
2023-11-13 00:29:16 +0700

    fix side bar status tracking work

--- my-app/src/utils/tools.js
+++ my-app/src/utils/tools.js
@@ -9,7 +9,7 @@ export const formatDate = (inputDate) => {
   const year = date.getFullYear();
   const formattedDay = day < 10 ? `0${day}` : day;
   const formattedMonth = month < 10 ? `0${month}` : month;
-  const formattedDate = `${formattedDay}/${formattedMonth}/${year}`;
+  const formattedDate = `${formattedDay}.${formattedMonth}.${year}`;
   return formattedDate;
 };
 
