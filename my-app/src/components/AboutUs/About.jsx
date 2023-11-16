e67bbc1331a1120a2cfc67130c318118b1f0a571
duongbao0803 <tonbao0803@gmail.com>
2023-11-17 00:25:19 +0700

    fix aboutus

--- my-app/src/components/AboutUs/About.jsx
+++ my-app/src/components/AboutUs/About.jsx
@@ -21,15 +21,23 @@ function About() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          backgroundColor: "orange",
         }}
       >
-        <div style={{ border: "1px solid black", padding: "10px" }}>
+        <div
+          style={{
+            padding: "10px",
+            backgroundColor: "white",
+          }}
+        >
           <h1>Project SWP391 - FPTU - Student Home Membership</h1>
-          <p>Instructor: PhuongLHK</p>
-          <p>4 Members</p>
+          <p>
+            <strong>Instructor</strong>: Thầy Lâm Hữu Khánh Phương
+          </p>
+          <strong>4 Members:</strong>
           <p>Dương Tôn Bảo - SE171065 </p>
           <p>Đặng Phan Gia Đức - SE171092 </p>
-          <p>Phạm Ngọc Bảo - SE171066 -</p>
+          <p>Phạm Ngọc Bảo - SE171066 </p>
 
           <p> Nguyễn Bình Phương Trâm - SE171076</p>
         </div>
