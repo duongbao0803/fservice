4d83c20909aebf1ec10bfee5d197344cda361f7f
duongbao0803 <tonbao0803@gmail.com>
2023-12-21 10:25:58 +0700

    fix login

--- my-app/src/components/Authen/Login.jsx
+++ my-app/src/components/Authen/Login.jsx
@@ -68,7 +68,7 @@ function Loginv2() {
             if (role === "ADMIN") {
               navigate("/error");
             } else if (role === "STAFF") {
-              navigate("/staff/work");
+              window.location.href = "/staff/work";
             } else {
               window.location.href = "/";
             }
